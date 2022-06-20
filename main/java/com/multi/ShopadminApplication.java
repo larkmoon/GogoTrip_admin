01dccece462a45a2faef55b25f8750889b2c582e
yunchanbin <21611273@yu.ac.kr>
2022-06-20 14:40:44 +0900

    first commit

---
+++ main/java/com/multi/ShopadminApplication.java
@@ -0,0 +1,13 @@
+package com.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopadminApplication.class, args);
+	}
+
+}
