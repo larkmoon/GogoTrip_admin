ac03be5782b07dc2fc1806a656661898b173f688
yunchanbin <21611273@yu.ac.kr>
2022-06-21 16:55:33 +0900

    admin first commit

--- main/java/com/multi/controller/AdminController.java
+++ main/java/com/multi/controller/AdminController.java
@@ -1,12 +1,19 @@
 package com.multi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.multi.biz.AdminBiz;
+import com.multi.vo.AdminVO;
+
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	
+	@Autowired
+	AdminBiz adbiz;
 	
 	@RequestMapping("add")
 	public String add(Model m) {
@@ -16,6 +23,18 @@ public class AdminController {
 	
 	@RequestMapping("select")
 	public String select(Model m) {
+		m.addAttribute("center", "admin/select");
+		return "index";
+	}
+	
+	@RequestMapping("registerimpl")
+	public String register(Model m, AdminVO obj) {
+		try {
+			adbiz.register(obj);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		m.addAttribute("center", "admin/select");
 		return "index";
 	}
