acc7537f6fceb63a6c23127a54f132d3315d61b6
yunchanbin <21611273@yu.ac.kr>
2022-06-22 10:42:19 +0900

    admin add 기능 구현

--- main/java/com/multi/controller/AJAXController.java
+++ main/java/com/multi/controller/AJAXController.java
@@ -1,8 +1,33 @@
 package com.multi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.multi.biz.AdminBiz;
+import com.multi.vo.AdminVO;
 
 @RestController
 public class AJAXController {
-
+	
+	@Autowired
+	AdminBiz adbiz;
+	
+	@RequestMapping("/checkid")
+	public String checkid(String id) {
+		AdminVO obj = null;
+		String result = "";
+		
+		try {
+			obj = adbiz.get(id);
+			if(obj == null) {
+				result = "0";
+			}else {
+				result = "1";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
 }
